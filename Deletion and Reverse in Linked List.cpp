1de95feb7d12e6eb5c721808ae93e10509529729
Aryen Rawat <aryen2323@gmail.com>
2022-09-30 22:26:49 +0530

    Create Deletion and Reverse in Linked List.cpp

---
+++ Deletion and Reverse in Linked List.cpp
@@ -0,0 +1,39 @@
+
+/* structure for a node 
+struct Node
+{
+	int data;
+	struct Node *next;
+};
+*/
+
+/* Function to delete a given node from the list */
+void deleteNode(struct Node **head, int key)
+{
+
+// Your code goes here
+   Node*ptr=*head,*qtr=(*head)->next;
+   while(qtr->data!=key){
+       ptr=ptr->next;
+       qtr=qtr->next;
+   }
+   ptr->next=qtr->next;
+   qtr->next=nullptr;
+}
+
+/* Function to reverse the linked list */
+ void reverse(struct Node** head_ref)
+{
+
+// Your code goes here
+
+Node*prev=nullptr,*curr=*head_ref;
+while(curr!=nullptr){
+    Node*temp=curr->next;
+    curr->next=prev;
+    prev=curr;
+    curr=temp;
+}
+*head_ref=prev->next;
+
+}
