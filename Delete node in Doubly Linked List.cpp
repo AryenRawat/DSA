b475156aba307d6b9a2c25ffeb2bba3b8b455e76
Aryen Rawat <aryen2323@gmail.com>
2022-09-30 22:28:22 +0530

    Create Delete node in Doubly Linked List.cpp

---
+++ Delete node in Doubly Linked List.cpp
@@ -0,0 +1,37 @@
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+*/
+
+class Solution
+{
+    public:
+    Node* deleteNode(Node *head_ref, int x)
+    {
+      //Your code here
+      Node*p=head_ref;
+      if(x==1){
+          p=p->next;
+          return p;
+      }
+      --x;
+      while(--x && p!=nullptr ){
+          p=p->next;
+      }
+      Node*temp = p->next;
+      p->next=p->next->next;
+      temp->prev=nullptr;
+      return head_ref;
+      
+    }
+};
