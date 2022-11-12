9f604b2e71bef1c09ffd1e53d96182bb297c4756
Aryen Rawat <aryen2323@gmail.com>
2022-11-12 07:56:50 +0530

    Create Queue Reversal.cpp

---
+++ Queues/Queue Reversal.cpp
@@ -0,0 +1,19 @@
+//function Template for C++
+
+//Function to reverse the queue.
+queue<int> rev(queue<int> q)
+{
+    // add code here.
+    queue<int>temp;
+    stack<int>help;
+    while(!q.empty()){
+        help.push(q.front());
+        q.pop();
+    }
+    while(!help.empty()){
+        temp.push(help.top());
+        help.pop();
+    }
+    return temp;
+    
+}
