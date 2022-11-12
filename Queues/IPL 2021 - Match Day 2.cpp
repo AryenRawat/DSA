832ced8c60dd5665af57961b3b40a7480456532e
Aryen Rawat <aryen2323@gmail.com>
2022-11-12 08:04:00 +0530

    Create IPL 2021 - Match Day 2.cpp

---
+++ Queues/IPL 2021 - Match Day 2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        // your code here
+        vector<int>vt;
+        deque<int>qt;
+        for(int i=0;i<n;i++){
+            
+            while(!qt.empty()&& qt.front()==i-k){
+                qt.pop_front();
+            }
+            
+            while(!qt.empty()&& arr[qt.back()]<=arr[i]){
+                qt.pop_back();
+            }
+            
+            qt.push_back(i);
+            if(i>=k-1){
+                vt.push_back(arr[qt.front()]);
+            }
+        }return vt;
+    }
+};
