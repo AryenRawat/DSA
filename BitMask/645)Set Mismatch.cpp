ad33260126af2135a23a1436be27ac49c04ba18a
Aryen Rawat <aryen2323@gmail.com>
2023-04-30 09:42:59 +0530

    Create 645)Set Mismatch.cpp

---
+++ BitMask/645)Set Mismatch.cpp
@@ -0,0 +1,31 @@
+//Using set
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        set<int>s;
+        int n=nums.size();
+        int req=(n*(n+1))/2;
+        int curr=0;
+        int a=0,b=0;
+        for(auto it:nums){
+            if(s.insert(it).second==0) a=it;
+        }
+        for(auto it:s) curr+=it;
+        b=req-curr;
+        return{a,b};
+    }
+};
+
+//using cyclic sort
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+       for(int i = 0; i<nums.size(); i++){
+            while(nums[i] != nums[nums[i] - 1])swap(nums[i], nums[nums[i] - 1]);
+        }
+        for(int i = 0; i<nums.size() ; i++){
+            if(nums[i] != i + 1)return {nums[i], i + 1};
+        }
+        return {-1,-1};
+    }
+};
