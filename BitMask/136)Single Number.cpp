71409847def8490485c2b7a5a6b6bc456401d49c
Aryen Rawat <aryen2323@gmail.com>
2023-04-30 09:09:57 +0530

    Create 136)Single Number.cpp

---
+++ BitMask/136)Single Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int curr=0;
+        for(auto it:nums){
+            curr^=it;
+        }
+        return curr;
+    }
+};
